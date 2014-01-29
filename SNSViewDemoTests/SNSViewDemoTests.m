913dc12692298bb5c92faa24630c8347af0875ac
hoanganh <hoanganh0986758785@gmail.com>
2014-01-29 17:08:52 +0900

    SNSViewDemo
    ソーシャル投稿機能デモ

---
+++ SNSViewDemoTests/SNSViewDemoTests.m
@@ -0,0 +1,36 @@
+//
+//  SNSViewDemo
+//  Version 1.0Tests.m
+//  SNSViewDemo
+//  Version 1.0Tests
+//
+//  Created by Hoang Anh on 2014/01/27.
+//  Copyright (c) 2014年 vnmobiapp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SNSViewDemoTests : XCTestCase
+
+@end
+
+@implementation SNSViewDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
